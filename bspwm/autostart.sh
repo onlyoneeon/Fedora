36acd9a19c3ec4dccf4be93b2e5abf18a5d185b0
onlyoneeon <87869280+onlyoneeon@users.noreply.github.com>
2022-05-24 19:38:19 +0100

    Add files via upload

---
+++ bspwm/autostart.sh
@@ -0,0 +1,9 @@
+$HOME/.config/polybar/launch.sh &
+
+run nm-applet &
+run pamac-tray &
+run xfce4-power-manager &
+numlockx on &
+blueberry-tray &
+picom --config $HOME/.config/bspwm/picom.conf &
+nitrogen --restore
